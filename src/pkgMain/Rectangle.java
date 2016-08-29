eb1a5a78d6951a199e308638ba242de0b52ba5d5
tstbag <tstbag@verizon.net>
2016-08-29 13:14:09 -0400

    initial commit

---
+++ src/pkgMain/Rectangle.java
@@ -0,0 +1,23 @@
+package pkgMain;
+
+public class Rectangle {
+
+	private double Length;
+	private double Width;
+	public Rectangle(double length, double width) {
+		Length = length;
+		Width = width;
+	}
+	public double getLength() {
+		return Length;
+	}
+	public double getWidth() {
+		return Width;
+	}
+	
+	public double Area()
+	{
+		// TODO Implement Area() function with the correct formula
+		return 0;
+	}	
+}
---
+++ src/pkgMain/cMain.java
@@ -0,0 +1,11 @@
+package pkgMain;
+
+public class cMain {
+
+	public static void main(String[] args) {
+
+		Rectangle rec = new Rectangle(1.5, 2.5);		
+		System.out.println("Rectangle Area: " + rec.Area());
+	}
+
+}
